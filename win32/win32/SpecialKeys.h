0544c36f3c6909611a9d0edd0d56dabc12f15aa4
Vlada Mamutova <36194644+VladaMamutova@users.noreply.github.com>
2020-05-26 14:22:08 +0300

    feat(dll): add a method of entering shortcut (special key + symbol)

--- win32/win32/SpecialKeys.h
+++ win32/win32/SpecialKeys.h
@@ -2,5 +2,9 @@
 
 enum SpecialKeys {
     BACKSPACE, // 0
-    ENTER // 1
+    ENTER, // 1
+    WIN, // 2
+    CTRL, // 3
+    SHIFT, // 4
+    ALT // 5
 };
--- win32/win32/win32.cpp
+++ win32/win32/win32.cpp
@@ -62,18 +62,10 @@ JNIEXPORT void JNICALL Java_main_kotlin_Win32_sendSymbol(JNIEnv* env,
     input.ki.wScan = symbol;
     input.ki.time = 0;
     input.ki.dwFlags = KEYEVENTF_UNICODE;
-    SendInput(1, &input, sizeof(INPUT));
+    SendInput(1, &input, sizeof(INPUT)); // press the key down
 
-    //INPUT input = { 0 };
-    //input.type = INPUT_KEYBOARD;
-
-    //input.ki.wVk = 0;
-    //input.ki.wScan = symb;
-    //input.ki.dwFlags = KEYEVENTF_UNICODE;
-    //SendInput(1, &input, sizeof(INPUT));
-
-    //input.ki.dwFlags = KEYEVENTF_KEYUP | KEYEVENTF_UNICODE;
-    //SendInput(1, &input, sizeof(INPUT));
+    input.ki.dwFlags |= KEYEVENTF_KEYUP;
+    SendInput(1, &input, sizeof(INPUT)); // release the key
 }
 
 JNIEXPORT void JNICALL Java_main_kotlin_Win32_sendSpecialKeys(JNIEnv* env,
@@ -111,4 +103,78 @@ JNIEXPORT void JNICALL Java_main_kotlin_Win32_sendSpecialKeys(JNIEnv* env,
 JNIEXPORT void JNICALL Java_main_kotlin_Win32_sendKeys(JNIEnv* env,
     jobject obj, jintArray specialKeys, jchar symbol) {
 
+    INPUT input = { 0 };
+    input.type = INPUT_KEYBOARD;
+    input.ki.wVk = 0;
+    input.ki.wScan = 0;
+    input.ki.time = 0;
+    input.ki.dwFlags = 0;
+
+    jsize len = env->GetArrayLength(specialKeys);
+    jint* specialKeysArray = env->GetIntArrayElements(specialKeys, NULL);
+    int* wVk = new int[len];
+    for (int i = 0; i < len; i++) {
+        wVk[i] = 0;
+        switch ((int)specialKeysArray[i])
+        {
+        case BACKSPACE: {
+            wVk[i] = VK_BACK;
+            break;
+        }
+        case ENTER: {
+            wVk[i] = VK_RETURN;
+            break;
+        }
+        case WIN: {
+            wVk[i] = VK_LWIN;
+            break;
+        }
+        case CTRL: {
+            wVk[i] = VK_CONTROL;
+            break;
+        }
+        case ALT: {
+            wVk[i] = VK_MENU;
+            break;
+        }
+        default:
+            break;
+        }
+
+        input.ki.wVk = wVk[i];
+        SendInput(1, &input, sizeof(INPUT)); // press the special key down
+    }
+
+    // Use virtual key codes to make key combinations work.
+    // The low-order byte of the return value of VkKeyScanExW contains
+    // the virtual-key code. GetKeyboardLayout(0) - get ID of the input
+    // language for the active thread.
+    short symbolVk = VkKeyScanExW(symbol, GetKeyboardLayout(0));
+    if (symbolVk != -1) {
+
+        input.ki.wVk = symbolVk & 0xFF;
+        SendInput(1, &input, sizeof(INPUT)); // press the key down
+
+        input.ki.dwFlags = KEYEVENTF_KEYUP;
+        SendInput(1, &input, sizeof(INPUT)); // release the key
+    } else {
+        input.ki.wVk = 0;
+        input.ki.wScan = symbol;
+        input.ki.dwFlags = KEYEVENTF_UNICODE;
+        ::SendInput(1, &input, sizeof(INPUT)); // press the key down
+
+        input.ki.dwFlags |= KEYEVENTF_KEYUP;
+        ::SendInput(1, &input, sizeof(INPUT)); // release the key
+    }
+
+    // release the special keys in reverse order
+    input.ki.dwFlags = KEYEVENTF_KEYUP;
+    for (int i = len - 1; i >= 0; i--)
+    {
+        input.ki.wVk = wVk[i];
+        SendInput(1, &input, sizeof(INPUT));
+    }
+
+    env->ReleaseIntArrayElements(specialKeys, specialKeysArray, NULL);
+    delete[] wVk;
 }
